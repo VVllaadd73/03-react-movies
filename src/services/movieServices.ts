bdfcc8cc118d62cd51220b348279ee2be291e16a
Vladislav <vvllaadd73@gmail.com>
2025-12-09 12:09:31 +0200

    FIX

--- src/services/movieServices.ts
+++ src/services/movieServices.ts
@@ -1,26 +1,26 @@
-// MovieServices.ts
+// movieServices.ts
 
-import axios from 'axios';
+import axios from "axios";
 
-import type Movie from '../types/movie';
+import type Movie from "../types/movie";
 
-const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+const BASE_URL = "https://api.themoviedb.org/3/search/movie";
 const API_KEY = import.meta.env.VITE_TMDB_TOKEN;
 
 interface moviesResponse {
-   results: Movie[];
+    results: Movie[];
 }
 
 export default async function fetchMovies(query: string): Promise<Movie[]> {
-   const configData = {
-      params: { query },
-      headers: {
-         accept: 'application/json',
-         Authorization: `Bearer ${API_KEY}`,
-      },
-   };
+    const configData = {
+        params: { query },
+        headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${API_KEY}`,
+        },
+    };
 
-   const response = await axios.get<moviesResponse>(BASE_URL, configData);
+    const response = await axios.get<moviesResponse>(BASE_URL, configData);
 
-   return response.data.results;
+    return response.data.results;
 }
